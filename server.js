b72c9fb9b8275a3b16d7d709e9fc16f989ce54d6
mmaesom <winifred.esomonu@stetis.com>
2022-12-18 06:54:45 +0000

    fixes

--- server.js
+++ server.js
@@ -25,10 +25,13 @@ const PORT = process.env.PORT || 5000;
 const dbURL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jjuc2ok.mongodb.net/?retryWrites=true&w=majority`;
 
 mongoose
-  .connect(dbURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb+srv://ccd-todo-app:LBU-CCD-Project-Todo-App-22!!!@cluster0.jjuc2ok.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() =>
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
   )
